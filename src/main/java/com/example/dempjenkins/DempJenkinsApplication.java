0ecf70b60e7340e58103ce4a38481b1040c77b73
Ragil Gilang Maulana <ragil.maulana-consultant@prudential.co.id>
2022-07-26 21:46:09 +0700

    first commit

---
+++ src/main/java/com/example/dempjenkins/DempJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dempjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DempJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DempJenkinsApplication.class, args);
+    }
+
+}
